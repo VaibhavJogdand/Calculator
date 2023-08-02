839afdfa90c09b808f8b9dbcf33f163018ee869c
vaibhav_jogdand <vrjogdand708@gmail.com>
2023-08-02 11:23:38 +0530

    add aws url

--- src/axios.js
+++ src/axios.js
@@ -5,7 +5,8 @@ import axios from "axios";
 // Create an instance of Axios with default configuration
 const Axios = axios.create({
 	// baseURL: "http://localhost:4000/api", // Replace 'your-backend-url' with the actual URL of your backend API
-	baseURL: "https://calculator-backend-q4mm.onrender.com/api", // Replace 'your-backend-url' with the actual URL of your backend API
+	// baseURL: "https://calculator-backend-q4mm.onrender.com/api", // Replace 'your-backend-url' with the actual URL of your backend API
+	baseURL: "http://3.111.144.26:4000/api", // AWS Backend
 });
 
 export default Axios;
